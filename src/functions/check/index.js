d1da6b7b95a320cc5223e800468011822a237137
xingyanhai <1101099728@qq.com>
2020-06-29 23:40:23 +0800

    添加云函数

---
+++ src/functions/check/index.js
@@ -0,0 +1,43 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async(event, context) => {
+  try {
+    console.log('待检测文本:' + event.content);
+    let result = await cloud.openapi.security.msgSecCheck({
+      content: event.content
+    })
+    console.log('result:' + JSON.stringify(result));
+
+    if (result && result.errCode.toString() === '87014') {
+      return {
+        code: 300,
+        msg: '内容含有违法违规内容',
+        data: result
+      }
+    } else {
+      return {
+        code: 200,
+        msg: 'ok',
+        data: result
+      }
+    }
+
+  } catch (err) {
+    if (err.errCode.toString() === '87014') {
+      return {
+        code: 300,
+        msg: '内容含有违法违规内容',
+        data: err
+      }
+    }
+    return {
+      code: 400,
+      msg: '调用security接口异常',
+      data: err
+    }
+  }
+}
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin([
+        {
+          from: path.join(__dirname, 'src/functions'),
+          to: path.join(__dirname, 'dist', process.env.NODE_ENV === 'production' ? 'build' : 'dev', process.env.UNI_PLATFORM, 'functions')
+        }
+      ])
+    ]
+  }
+}
